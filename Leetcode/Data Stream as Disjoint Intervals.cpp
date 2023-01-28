3f114bc37d1316b07700ead12b3af3371fead2fa
Nirali Rathod <niralirathod28@gmail.com>
2023-01-28 17:25:29 +0530

    Jan 28 daily challenge

---
+++ Leetcode/Data Stream as Disjoint Intervals.cpp
@@ -0,0 +1,78 @@
+
+
+/* 
+    Solution by Nirali Rathod
+    
+    ***********************************************************
+
+Given a data stream input of non-negative integers a1, a2, ..., an, summarize the numbers seen so far as a list of disjoint intervals.
+
+Implement the SummaryRanges class:
+
+SummaryRanges() Initializes the object with an empty stream.
+void addNum(int value) Adds the integer value to the stream.
+int[][] getIntervals() Returns a summary of the integers in the stream currently as a list of disjoint intervals [starti, endi]. The answer should be sorted by starti.
+ 
+
+Example 1:
+
+Input
+["SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals", "addNum", "getIntervals"]
+[[], [1], [], [3], [], [7], [], [2], [], [6], []]
+Output
+[null, null, [[1, 1]], null, [[1, 1], [3, 3]], null, [[1, 1], [3, 3], [7, 7]], null, [[1, 3], [7, 7]], null, [[1, 3], [6, 7]]]
+
+Explanation
+SummaryRanges summaryRanges = new SummaryRanges();
+summaryRanges.addNum(1);      // arr = [1]
+summaryRanges.getIntervals(); // return [[1, 1]]
+summaryRanges.addNum(3);      // arr = [1, 3]
+summaryRanges.getIntervals(); // return [[1, 1], [3, 3]]
+summaryRanges.addNum(7);      // arr = [1, 3, 7]
+summaryRanges.getIntervals(); // return [[1, 1], [3, 3], [7, 7]]
+summaryRanges.addNum(2);      // arr = [1, 2, 3, 7]
+summaryRanges.getIntervals(); // return [[1, 3], [7, 7]]
+summaryRanges.addNum(6);      // arr = [1, 2, 3, 6, 7]
+summaryRanges.getIntervals(); // return [[1, 3], [6, 7]]
+
+    ***********************************************************
+*/
+
+
+#include <bits/stdc++.h>
+
+class SummaryRanges {
+public:
+    set<int> s;
+    SummaryRanges() {
+        
+    }
+    
+    void addNum(int value) {
+        s.insert(value);
+    }
+    
+    vector<vector<int>> getIntervals() {
+        vector<vector<int>> intervals;
+        int start = *s.begin();
+        int end = *s.begin();
+        for(auto i=++s.begin();i!=s.end();i++) {
+            int val = *i;
+            if(val-end == 1)
+                end = val;
+            else {
+                intervals.push_back({start,end});
+                start = end = val;
+            }
+        }
+        intervals.push_back({start,end});
+        return intervals;
+    }
+};
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges* obj = new SummaryRanges();
+ * obj->addNum(value);
+ * vector<vector<int>> param_2 = obj->getIntervals();
+ */
