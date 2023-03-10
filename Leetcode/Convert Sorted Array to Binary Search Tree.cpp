6c7bb818b017695c8d8604e3f2160dabdb8e9ad7
Nirali Rathod <niralirathod28@gmail.com>
2023-03-10 18:37:04 +0530

    Binary Search problems

---
+++ Leetcode/Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,60 @@
+
+
+/* 
+    Solution by Nirali Rathod
+    
+    ***********************************************************
+
+Given an integer array nums where the elements are sorted in ascending order, convert it to a 
+height-balanced binary search tree.
+
+Example 1:
+
+https://assets.leetcode.com/uploads/2021/02/18/btree1.jpg
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+https://assets.leetcode.com/uploads/2021/02/18/btree2.jpg
+
+Example 2:
+
+https://assets.leetcode.com/uploads/2021/02/18/btree.jpg
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,null,3] and [3,1] are both height-balanced BSTs.
+
+    ***********************************************************
+*/
+
+
+#include <bits/stdc++.h>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* balancedBST(vector<int>& nums,int start,int end) {
+        if(start > end)
+            return NULL;
+        
+        int mid = (start + end)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = balancedBST(nums,start,mid-1);
+        root->right = balancedBST(nums,mid+1,end);
+
+        return root;
+
+    }
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return balancedBST(nums,0,nums.size()-1);
+    }
+};
