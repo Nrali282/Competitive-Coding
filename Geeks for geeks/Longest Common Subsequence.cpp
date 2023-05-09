61c0ea6e69fd28cbacfffa0fdae19e3acbe229ce
Nirali Rathod <niralirathod28@gmail.com>
2023-05-09 13:36:05 +0100

    LCS Problems

---
+++ Geeks for geeks/Longest Common Subsequence.cpp
@@ -0,0 +1,90 @@
+
+
+/* 
+    Solution by Nirali Rathod
+    
+    ***********************************************************
+Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+Example 1:
+
+Input:
+A = 6, B = 6
+str1 = ABCDGH
+str2 = AEDFHR
+Output: 3
+Explanation: LCS for input Sequences
+“ABCDGH” and “AEDFHR” is “ADH” of
+length 3.
+
+Example 2:
+
+Input:
+A = 3, B = 2
+str1 = ABC
+str2 = AC
+Output: 2
+Explanation: LCS of "ABC" and "AC" is
+"AC" of length 2.
+
+    ***********************************************************
+*/
+
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+const int mod=1e9+7;
+using namespace std;
+
+// } Driver Code Ends
+// function to find longest common subsequence
+
+class Solution
+{
+    public:
+    //Function to find the length of longest common subsequence in two strings.
+    int lcs(int x, int y, string s1, string s2)
+    {
+        int n = s1.size();
+        int m = s2.size();
+        
+        int dp[n+1][m+1];
+        
+        for(int i=0;i<n+1;i++) {
+            dp[i][0] = 0;
+        }
+        
+        for(int i=0;i<m+1;i++) {
+            dp[0][i] = 0;
+        }
+        
+        for(int i=1;i<n+1;i++) {
+            for(int j=1;j<m+1;j++) {
+                if(s1[i-1] == s2[j-1])
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t,n,k,x,y;
+    cin>>t;
+    while(t--)
+    {
+        cin>>x>>y;          // Take size of both the strings as input
+        string s1,s2;
+        cin>>s1>>s2;        // Take both the string as input
+        Solution ob;
+        cout << ob.lcs(x, y, s1, s2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
