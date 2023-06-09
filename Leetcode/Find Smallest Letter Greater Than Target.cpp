9b8fb01222c562ad8c0351bd6b4678c58916e316
Nirali Rathod <niralirathod28@gmail.com>
2023-06-09 10:12:24 +0530

    Jun 9 daily challenge

---
+++ Leetcode/Find Smallest Letter Greater Than Target.cpp
@@ -0,0 +1,50 @@
+
+
+/* 
+    Solution by Nirali Rathod
+    
+    ***********************************************************
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+
+Example 3:
+
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+
+    ***********************************************************
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int i = 0, j = letters.size()-1, mid;
+        while(i <= j) {
+            int mid = (i+j)/2;
+            if(letters[mid] <= target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return i == letters.size() ? letters[0] : letters[i];
+    }
+};
