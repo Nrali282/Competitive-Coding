39b6869fb1ae9343c4944ae016016a36981600ec
Nirali Rathod <niralirathod28@gmail.com>
2023-06-27 10:13:13 +0530

    Jun 27 daily challenge

---
+++ Leetcode/Find K Pairs with Smallest Sums.cpp
@@ -0,0 +1,73 @@
+
+
+/* 
+    Solution by Nirali Rathod
+    
+    ***********************************************************
+You are given two integer arrays nums1 and nums2 sorted in ascending order and an integer k.
+
+Define a pair (u, v) which consists of one element from the first array and one element from the second array.
+
+Return the k pairs (u1, v1), (u2, v2), ..., (uk, vk) with the smallest sums.
+
+Example 1:
+
+Input: nums1 = [1,7,11], nums2 = [2,4,6], k = 3
+Output: [[1,2],[1,4],[1,6]]
+Explanation: The first 3 pairs are returned from the sequence: [1,2],[1,4],[1,6],[7,2],[7,4],[11,2],[7,6],[11,4],[11,6]
+
+Example 2:
+
+Input: nums1 = [1,1,2], nums2 = [1,2,3], k = 2
+Output: [[1,1],[1,1]]
+Explanation: The first 2 pairs are returned from the sequence: [1,1],[1,1],[1,2],[2,1],[1,2],[2,2],[1,3],[1,3],[2,3]
+
+Example 3:
+
+Input: nums1 = [1,2], nums2 = [3], k = 3
+Output: [[1,3],[2,3]]
+Explanation: All possible pairs are returned from the sequence: [1,3],[2,3]
+
+    ***********************************************************
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        int m = nums2.size();
+
+        vector<vector<int>> res;
+        set<pair<int, int>> visited;
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>> minHeap;
+
+        minHeap.push({nums1[0] + nums2[0], {0, 0}});
+        visited.insert({0, 0});
+        
+        while (k-- && !minHeap.empty()) {
+            auto top = minHeap.top();
+            minHeap.pop();
+            int i = top.second.first;
+            int j = top.second.second;
+
+            res.push_back({nums1[i], nums2[j]});
+
+            if (i + 1 < n && visited.find({i + 1, j}) == visited.end()) {
+                minHeap.push({nums1[i + 1] + nums2[j], {i + 1, j}});
+                visited.insert({i + 1, j});
+            }
+
+            if (j + 1 < m && visited.find({i, j + 1}) == visited.end()) {
+                minHeap.push({nums1[i] + nums2[j + 1], {i, j + 1}});
+                visited.insert({i, j + 1});
+            }
+        }
+        return res;
+    }
+};
